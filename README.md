1e361669ca152045355e08a4d874116732f26a1f
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2018-03-24 17:44:32 -0400

    propName -> propPath; added dictPath; added decorate(); moved transformers to own file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # synonomous
 Transform strings into synonyms for list decoration
 
+"... because `synonymous` was already registered!"
+
 ### Get some synonyms
 To just get a list of synonyms for a given string using the default selection of "transformer" functions (`verbatim` and `toCamelCase`):
 ```js
@@ -15,7 +17,98 @@ Returns:
     'backgroundColor'
 ]
 ```
+To temporarily override the default list of "transformer" functions for a single call to `getSynonyms`:
+```js
+var Synonomous = require('synonomous');
+var synonomous = new Synonomous;
+synonomous.getSynonyms('background-color', ['toAllCaps']);
+```
+Returns:
+```js
+[
+   'BACKGROUND_COLOR'
+]
+```
+#### Custom instance default
+To set the default list of "transformer" functions for all subsequent calls from this instance:
+```js
+var customTransformationsList = ['verbatim', 'toAllCaps'];
+synonomous.transformations = customTransformationsList;
+```
+#### Reset instance default
+To reset to default:
+```js
+delete synonomous.transformations; // reveal Synonomous.prototype.transformations
+```
+#### Custom shared default
+To override default for all subsequent calls from all instances:
+To set the default list of "transformer" functions for all subsequent calls from this instance:
+```js
+var saveSharedDefault = synonomous.prototype.transformations; // save for later
+synonomous.prototype.transformations = customTransformationsList;
+...
+// restore shared default later on:
+synonomous.prototype.transformations = saveSharedDefault;
+```
 
+### Add a custom transformer
+```js
+var transformers = require('synonomous/transformers');
+transformers.toQuotation = function(key) { return '"' + key + '"'; }
+
+### Decorate an object with properties
+To add properties all referencing another existing property (_i.e.,_ synonyms):
+```js
+var myObject = {}, item = {};
+var propNames = ['width', 'thickness'];
+synonomous.decorate(myObject, propNames, item);
+```
+Returns mutated `myObject`:
+```
+{
+    'width': item,
+    'thickness': item
+}
+```
+Note that this function does not call `getSynonyms`; it merely adds properties to `myObject` with the names provided in `propNames` and sets them to `item`.
+
+### Decorate an object property
+Rather than adding the properties to `myObject` in the above example, you can also specify a property of `myObject` to decorate instead by defining `synonomous.dictPath` (which is undefined by default).
+The target property is expected to be an object. If it does not exist, it will be created as an object (with null prototype).
+Example:
+```js
+synonymous.dictPath = 'properties';
+synonomous.decorate(myObject, propNames, item);
+```
+Returns:
+```
+{
+    'properties': {
+        'width': item,
+        'thickness': item
+    }
+}
+```
+`dictPath` can also be a dot-path:
+```js
+synonymous.dictPath = 'properties.dimensions'; // or: ['properties'.'dimensions']
+synonomous.decorate(myObject, propNames, item);
+```
+Returns:
+```
+{
+    'properties': {
+        'dimensions': {
+            'width': item,
+            'thickness': item
+        }
+    }
+}
+```
+It is interesting to note that getting `synonymous.dictPath` always returns an array, but one with a `toString`
+override such that when coerced to a string it returns dot-separated values, producing a dot-path.
+
+`synonomous.dictPath` can be customized in the same manner as described above for `synonomous.transformations`.
 ### Decorate an array with synonyms
 This is the real utility of the `synonomous` module:
 ```js
@@ -35,7 +128,7 @@ This call decorates and returns `list` which would then look like this:
     'backgroundColor': list[1] // camelCase
  }
 ```
-Spcifically, the array now has properties that point to elements whose keys are transformations of each element. This lets the array double as a dictionary to its own elements.
+Specifically, the array now has properties that point to elements whose keys are transformations of each element. This lets the array double as a dictionary to its own elements.
 
 Note that this is not an enumeration; the property values are not integer indexes but rather references to the elements themselves.
 
@@ -58,7 +151,7 @@ synonomous.decorateList(1, list); // just decorate with synonyms for 2nd element
  }
 ```
 
-#### Decorating with synonyms of a specified property of each element
+#### Decorating with a property of an element
 When elements are objects rather than string primitives, you can specify which property of such objects to make synonyms of with the overload `decorateList(list: (string|object)[], propName: string = 'name')`:
 ```js
 var synonomous = new Synonomous;
@@ -66,7 +159,7 @@ var list = [
     { style: 'borderLeft', value: '8px' },
     { style: 'background-color', value: 'pink' }
 ];
-synonomous.decorateList(list, 'style'); // decorate with synonyms of `style` property of elements
+synonomous.decorateList(list, 'style'); // decorate with synonyms of value of each element's `style` property
 ```
 `list` now looks like this:
 ```js
@@ -78,7 +171,10 @@ synonomous.decorateList(list, 'style'); // decorate with synonyms of `style` pro
     'backgroundColor': list[1] // camelCase
  }
 ```
-Note that `propName` defaults to `'name'` when not given and the list element is an object rather than a string primitive.
+Notes:
+1. The `propName` parameter defaults to the value of the `synonomous.propName` property when omitted. When so defined, _and_ the list element is an object, `propName` is used to drill down into the list element. Otherwise the list element iteself (coerced to a string) is used as the source for the synonyms.
+2. Both the `propName` parameter and the `synonomous.propName` property can be a dot-path (or an array), similar to `synonomous.dictPath`.
+3. `synonomous.propName` can be customized in the same manner as described above for `synonomous.dictPath` and `synonomous.transformations`.
 
 #### Decorating with synonyms of a property of a single element
 You can of course combine these two features with the overload `decorateList(index: number, list: object[], propName: string = 'name')`:
@@ -95,5 +191,11 @@ The `verbatim` transformer does not prepend `$`. Any results that are integers t
 
 ### Revision History
 
+* **2.0.0**
+   * Added `decorate` method and `dictPath` property.
+   * Changed `propName` property to `propPath`. _(Breaking change if property used.)_
+   * Changed optional constructor params to a single `options` object. _(Breaking change if params were used.)_
+   * Moved transformers to their own file. _(Breaking change if you want to override and exsiting transformer or add a custom transformer.)_
+* **1.0.2** - Fixed overloads of `decorateList` when first param `index` is omitted.
 * **1.0.1** — Added `toTitle` transformer.
 * **1.0.0** — Initial release.
--- index.js
+++ index.js
@@ -1,48 +1,27 @@
 'use strict';
 
-// all instances of xX or _X
-var REGEX_CAMEL_CASE_OR_UNDERSCORE = /([^_A-Z])([A-Z]+)/g;
-var REGEX_ALL_PUNC_RUN = /[^a-z0-9]+/gi;
+var transformers = require('./transformers');
 
-// all instances of _x
-var REGEX_ALL_PUNC_RUN_BEFORE_LETTER = /[^a-z0-9]+([a-z0-9])?/ig;
-function WITH_UPPER_CASE(match, char) { return char === undefined ? '' : char.toUpperCase(); }
-
-var REGEX_INITIAL_DIGIT = /^(\d)/;
-var WITH_DOLLAR_PREFIX = '$$$1';
-
-var REGEX_INITIAL_CAPITAL = /^([A-Z])/;
-function WITH_LOWER_CASE(match, char) { return char.toLowerCase(); }
-
-var REGEXP_LOWER_CASE_LETTER = /[a-z]/;
-var REGEXP_WORD_SEPARATORS = /[\s\-_]*([^\s\-_])([^\s\-_]+)/g;
-var WITH_CAPTIAL_LETTER = function(a, b, c) { return b.toUpperCase() + c; };
-
-var REGEXP_CAPITAL_LETTERS = /[A-Z]+/g;
-var WITH_PREFIXED_SPACE = ' $&';
-
-var REGEXP_OVER_CAPITALIZED_WORDS = /([A-Z]+)([A-Z][a-z])/g;
-var WITH_SEPARATE_WORDS = '$1 $2';
-
-/** @typedef {function} Transformer
- * @param {string} key
- * @returns {string}
- */
+var optionNames = ['transformations', 'propPath', 'dictPath'];
 
 /**
  * @classdesc This object holds a list of transformations used by {@link Synonomous.prototype.getSynonyms} and {@link Synonomous.prototype.decorateList}.
  *
  * Additional transformer functions may be mixed into the prototype (or added to an instance).
  *
- *  @param {string[]} [transformations] - If omitted, {@link Synonomous.prototype.transformations} serves as a default.
+ * @param {object} [options]
+ * @param {string[]} [transformations] - If omitted, {@link Synonomous.prototype.transformations} serves as a default.
+ * @param {string} [propPath] - If omitted, {@link Synonomous.prototype.propPath} serves as a default.
+ * @param {string} [dictPath] - If omitted, {@link Synonomous.prototype.dictPath} serves as a default.
  * @constructor
  */
-function Synonomous(transformations, propName) {
-    if (transformations) {
-        this.transformations = transformations;
-    }
-    if (propName) {
-        this.propName = propName;
+function Synonomous(options) {
+    if (options) {
+        optionNames.forEach(function(key) {
+            if (options[key]) {
+                this[key] = options[key];
+            }
+        }, this);
     }
 }
 
@@ -52,7 +31,8 @@ Synonomous.prototype = {
     /**
      * @summary Default list of active registered transformations by name.
      * @desc Used by {@link Synonomous.prototype.getSynonyms} and {@link Synonomous.prototype.decorateList}.
-     * An override may be defined on the instance, easily done by supplying to constructor's 1st parameter.
+     *
+     * An override may be defined on the instance, easily done by supplying as a constructor option.
      *
      * This is a global default; mutate only if you want to change the default for all your instances.
      * @see {@link Synonomous.prototype.verbatim}
@@ -67,74 +47,58 @@ Synonomous.prototype = {
     ],
 
     /**
-     * @summary Default property name when list elements are objects.
+     * @summary Drill down path for name to make synonyms of.
      * @desc Used by {@link Synonomous.prototype.decorateList}.
-     * An override may be defined on the instance, easily done by supplying to constructor's 2nd parameter.
      *
-     * This is a global default; mutate only if you want to change the default for all your instances.
-     * @default
-     * @type {string[]}
+     * This is the default property dot-path within each list element to find the value to make synonyms of.
+     * If undefined (and no temporary override is given in the call to `decorateList`),
+     * the element value itself (coerced to a string) is used to make the synonyms.
+     *
+     * The setter accepts any falsy value to undefine; or a string of dot-separated parts; or an array of parts.
+     *
+     * The getter always returns an array with a `toString` override that returns dot-separated string when coerced to a string.
+     *
+     * An override may be defined on the instance, easily done by supplying as a constructor option.
+     *
+     * The global default for all instances can be reset using the setter with the prototype as the execution context,
+     * _e.g._ `Synonomous.prototype.propPath = newValue;`.
+     *
+     * @type {undefined|string[]}
      * @memberOf Synonomous#
      */
-    propName: 'name',
-
-    /** A transformer that returns its input converted to a string with ` + '' `.
-     * @memberOf Synonomous#
-     */
-    verbatim: function(key) {
-        return key + '';
+    set propPath(crumbs) {
+        this._propPath = newBreadcrumbs(crumbs);
     },
-
-    /** A transformer that converts runs of punctuation (non-alphanumerics, actually) to "camelCase" by removing such runs and capitalizing the first letter of each word.
-     * The first letter of the first word is forced to lower case.
-     * Otherwise, leaves other letters' case as they were.
-     *
-     * When the result begins with a digit, it's prefixed with with `$` for two reasons:
-     * 1. To avoid conflicts with array element indexes.
-     * 2. To create an identifier that can be used to the right of the dot (`.`) dereferencing operator (identifiers cannot start with a digit but can contain a `$`).
-     *
-     * @type {Transformer}
-     * @memberOf Synonomous#
-     */
-    toCamelCase: function(key) {
-        return key
-            .replace(REGEX_ALL_PUNC_RUN_BEFORE_LETTER, WITH_UPPER_CASE)
-            .replace(REGEX_INITIAL_DIGIT, WITH_DOLLAR_PREFIX)
-            .replace(REGEX_INITIAL_CAPITAL, WITH_LOWER_CASE);
+    get propPath() {
+        return this._propPath;
     },
-
-    /** A transformer that converts all runs of punctuation (non-alphanumerics, actually), as well as all camel case transitions, to underscore.
-     * Results are converted to all caps.
-     *
-     * When the result begins with a digit, it's prefixed with with `$` for two reasons:
-     * 1. To avoid conflicts with array element indexes.
-     * 2. To create an identifier that can be used to the right of the dot (`.`) dereferencing operator (identifiers cannot start with a digit but can contain a `$`).
-     *
-     * @type {Transformer}
-     * @memberOf Synonomous#
-     */
-    toAllCaps: function(key) {
-        return key
-            .replace(REGEX_ALL_PUNC_RUN, '_')
-            .replace(REGEX_CAMEL_CASE_OR_UNDERSCORE, '$1_$2')
-            .replace(REGEX_INITIAL_DIGIT, WITH_DOLLAR_PREFIX)
-            .toUpperCase();
-    },
+    _propPath: ['name'], // default for all instances
 
     /**
-     * A transformer that separates camel case or white-space-, hyphen-, or underscore-separated-words into truly separate words and capitalizing the first letter of each.
+     * @summary Default path to property to decorate; or undefined to decorate the object itself.
+     * @desc Used by {@link Synonomous.prototype.decorate} and {@link Synonomous.prototype.decorateList}.
      *
-     * This transformer is meant to create column headers from column names. It deliberating inserts spaces so the results are unsuitable as JavaScript identifiers.
-     * @type {Transformer}
+     * The setter accepts any falsy value to undefine; or a string of dot-separated parts; or an array of parts.
+     *
+     * The getter always returns an array with a `toString` override that returns dot-separated string when coerced to a string.
+     *
+     * If undefined, decorations are placed in `list[this.dictPath[0]][this.dictPath[1]][etc]`; else decorations are placed directly on `list` itself.
+     *
+     * An override may be defined on the instance, easily done by supplying to as a constructor option.
+     *
+     * The global default for all instances can be reset using the setter with the prototype as the execution context,
+     * _e.g._ `Synonomous.prototype.dictPath = newValue;`.
+     *
+     * @type {undefined|string|string[]}
      * @memberOf Synonomous#
      */
-    toTitle: function(key) {
-        return (REGEXP_LOWER_CASE_LETTER.test(key) ? key : key.toLowerCase())
-            .replace(REGEXP_WORD_SEPARATORS, WITH_CAPTIAL_LETTER)
-            .replace(REGEXP_CAPITAL_LETTERS, WITH_PREFIXED_SPACE)
-            .replace(REGEXP_OVER_CAPITALIZED_WORDS, WITH_SEPARATE_WORDS)
-            .trim();
+    set dictPath(crumbs) {
+        this._dictPath = newBreadcrumbs(crumbs);
     },
+    get dictPath() {
+        return this._dictPath;
+    },
+    _dictPath: [], // default for all instances, settable by Synonomous.prototype.dictPath setter
 
     /**
      * If `name` is a string and non-blank, returns an array containing unique non-blank converted names.
@@ -146,24 +110,42 @@ Synonomous.prototype = {
         var synonyms = [];
         if (typeof name === 'string' && name) {
             (transformations || this.transformations).forEach(function(key) {
-                if (typeof this[key] !== 'function') {
+                if (typeof transformers[key] !== 'function') {
                     throw new ReferenceError('Unknown transformer "' + key + '"');
                 }
-                var synonym = this[key](name);
+                var synonym = transformers[key](name);
                 if (synonym !== '' && !(synonym in synonyms)) {
                     synonyms.push(synonym);
                 }
-            }, this);
+            });
         }
         return synonyms;
     },
 
     /**
+     * Decorate a `collection` with properties named in `propNames` all referencing `item`.
+     * @param {object} collection - The object to decorate. If `this.dictPath` is defined, then decorate `collection[this.dictPath]` instead (created as needed).
+     *
+     * @param {string[]} propNames
+     * @param item
+     * @returns {object} `collection`
+     */
+    decorate: function(collection, propNames, item) {
+        collection = drilldown(collection, this.dictPath);
+        propNames.forEach(function(propName) {
+            if (!(propName in collection)) {
+                collection[propName] = item;
+            }
+        });
+        return collection;
+    },
+
+    /**
      * @summary Add dictionary synonyms to an array.
      *
-     * @desc Adds synonyms for a single element (`index`) or the entire array, based on a given property of each element (`propName`) or the element itself.
+     * @desc Adds synonyms for a single element (`index`) or the entire array, based on a given property of each element (`propPath`) or the element itself.
      *
-     * That is, each element is either converted to a string or is an object with a property named `propName` converted to a string.
+     * That is, each element is either converted to a string or is an object with a property named `propPath` converted to a string.
      *
      * All transformers named in `transformations` are run on that string and all the resulting unique non-blank "synonyms" are added as properties to the array with the value of the property being a reference to the element (if it was an object) or a copy of the element (if it was a string), subject to the following rules:
      * 1. Duplicate synonyms are not added.
@@ -172,32 +154,56 @@ Synonomous.prototype = {
      * @param {number} [index] - Index of element of `list` to add synonyms for. If omitted:
      * 1. Adds synonyms for all elements of `list`.
      * 2. `list` and `index` are promoted to the 1st and 2nd parameter positions, respectively.
-     * @param {(string|Object.<string, string>)[]} list - Array to decorate _and_ whose element(s) to make synonyms of.
-     * @param {string} [propName] - Name of the property in each element of `list` to make synonyms of. If omitted adds synonyms of the list element itself if a string or of the list element's `name` property if an object.
+     * @param {(string|Object.<string, string>)[]} list - Array whose element(s) to make synonyms of _and_ the object to decorate. If `this.dictPath` is defined, then decorate `list[this.dictPath]` instead (created as needed).
+     * @param {string} [propPath=this.propPath] - Name of the property in each element of `list` to make synonyms of. If defined _and_ list element is an object, adds synonyms of `list[propPath]` as string; else adds synonyms of the list element itself as string.
      * @returns {Array} `list`
      * @memberOf Synonomous#
      */
-    decorateList: function(index, list, propName) {
+    decorateList: function(index, list, propPath) {
         var elements;
         if (typeof index === 'number') {
             elements = [list[index]];
         } else {
             // promote args
             list = elements = arguments[0];
-            propName = arguments[1];
+            propPath = arguments[1];
         }
-        propName = propName || this.propName;
+        propPath = propPath ? newBreadcrumbs(propPath) : this.propPath;
         elements.forEach(function(item) {
-            var name = typeof item !== 'object' ? item : item[propName];
-            this.getSynonyms(name).forEach(function(synonym) {
-                synonym = synonym.replace(REGEX_INITIAL_DIGIT, WITH_DOLLAR_PREFIX);
-                if (!(synonym in list)) {
-                    list[synonym] = item;
-                }
-            });
+            var value = propPath !== undefined && typeof item === 'object' ? drilldown(item, propPath) : item;
+            var synonyms = this.getSynonyms(value);
+            this.decorate(list, synonyms, item);
         }, this);
         return list;
     }
 };
+
+function drilldown(collection, breadcrumbs) {
+    return breadcrumbs.reduce(function(result, crumb) {
+        return result[crumb] || (result[crumb] = Object.create(null));
+    }, collection);
+}
+
+function newBreadcrumbs(crumbs) {
+    if (!crumbs) {
+        crumbs = [];
+    } else if (Array.isArray(crumbs)) {
+        crumbs = crumbs.slice();
+    } else {
+        crumbs = (crumbs + '').split('.');
+    }
+
+    crumbs.toString = crumbsToString;
+
+    return crumbs;
+}
+
+/**
+ * @this {Array}
+ * @returns {string}
+ */
+function crumbsToString() {
+    return this.join('.');
+}
 
 module.exports = Synonomous;
